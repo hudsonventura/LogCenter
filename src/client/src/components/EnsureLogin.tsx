6e8b3db6845d1c057d4cdbbe1cd3dbd330ed6a79
Hudson Ventura <hudsonventura@outlook.com>
2025-03-18 21:50:25 -0400

    Fixing error when token was expired

--- src/client/src/components/EnsureLogin.tsx
+++ src/client/src/components/EnsureLogin.tsx
@@ -1,16 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import api from "@/services/api";
 
 export default function EnsureLogin() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-        if (!sessionStorage.getItem("token")) {
-            navigate("/login");
-        }
-  }, []);
-
-  
+	const navigate = useNavigate();
 
 
+	useEffect(() => {
+		const checkLogin = async () => {
+			if (!sessionStorage.getItem("token")) {
+				navigate("/login");
+				return;
+			}
+
+			try {
+				console.log("Aqui 1")
+				const response = await api.post("/logoff", {});
+
+			} catch (error) {
+				if(error.status === 401){
+					console.log("Aqui 2")
+					navigate("/login");
+					sessionStorage.removeItem("token");
+				}
+			}
+		};
+
+	}, []);
+
+
+
+	return <></>;
 }
+
